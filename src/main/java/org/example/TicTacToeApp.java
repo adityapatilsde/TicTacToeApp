4bea3896e04f526e5df95961f6322f97eba8b044
aditya <adityapatilsde@gmail.com>
2026-04-21 21:51:56 +0530

    use case 3 completed

--- src/main/java/org/example/TicTacToeApp.java
+++ src/main/java/org/example/TicTacToeApp.java
@@ -1,35 +1,25 @@
 package org.example;
 
-import java.util.Random;
+import java.util.Scanner;
 
 public class TicTacToeApp {
 
+
+    public static int getUserMove() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int slot;
+        System.out.print("Enter your move (1-9): ");
+        slot = scanner.nextInt();
+
+        return slot;
+    }
+
     public static void main(String[] args) {
-        String user = "User";
-        String computer = "Computer";
-        char userSymbol;
-        char computerSymbol;
-        String currentPlayer;
-        Random random = new Random();
-        int toss = random.nextInt(2); // 0 or 1
-
-        System.out.println("Tossing a coin...");
-        if (toss == 0) {
-            System.out.println("User won the toss!");
-
-            currentPlayer = user;
-            userSymbol = 'X';
-            computerSymbol = 'O';
-
-        } else {
-            System.out.println("Computer won the toss!");
-
-            currentPlayer = computer;
-            computerSymbol = 'X';
-            userSymbol = 'O';
-        }
-        System.out.println("First Player: " + currentPlayer);
-        System.out.println("User Symbol: " + userSymbol);
-        System.out.println("Computer Symbol: " + computerSymbol);
+        int userSlot = getUserMove();
+        System.out.println("User selected slot: " + userSlot);
     }
 }
+
+
