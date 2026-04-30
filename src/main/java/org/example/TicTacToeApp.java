273ba0e7428a481499eb4cc338307514f7c09a08
aditya <adityapatilsde@gmail.com>
2026-04-30 21:41:36 +0530

    use case 6 completed tic tac toe app

--- src/main/java/org/example/TicTacToeApp.java
+++ src/main/java/org/example/TicTacToeApp.java
@@ -2,33 +2,13 @@ package org.example;
 
 public class TicTacToeApp {
 
-    // Method to check if move is valid
-    public static boolean isValidMove(char[][] board, int row, int col) {
-
-        // Step 1: Check boundary conditions
-        if (row < 0 || row > 2 || col < 0 || col > 2) {
-            return false;
-        }
-        if (board[row][col] != '-') {
-            return false;
-        }
-        return true;
-    }
+    static char[][] board = new char[3][3];
 
     public static void main(String[] args) {
-        char[][] board = {
-                {'-', '-', '-'},
-                {'-', '-', '-'},
-                {'-', '-', '-'}
-        };
-
-        int row = 1;
-        int col = 1;
-        boolean result = isValidMove(board, row, col);
-        if (result) {
-            System.out.println("Move is valid");
-        } else {
-            System.out.println("Move is invalid");
-        }
+        placeMove(0, 0, 'X');
+        System.out.println("Value at (0,0): " + board[0][0]);
+    }
+    static void placeMove(int row, int col, char symbol) {
+        board[row][col] = symbol;
     }
 }
